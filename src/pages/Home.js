862db3297907849cca57d0c53a72601b9cbe6c7a
aakaaashmann <aks5060708090100@gmail.com>
2024-06-21 10:50:11 +0530

    init useSelector useEffect and get all cars

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import DefaultLayout from '../components/DefaultLayout'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllCars } from '../redux/actions/carsActions'
 
 
 function Home() {
+    const {cars} = useSelector(state=>state.carsReducer)
+    const dispatch = useDispatch()
+
+
+    useEffect(() => {
+        dispatch(getAllCars())
+    },[])
+
     return (
         <DefaultLayout>
             <h1>Home Page</h1>
+            <h1>The length of cars array is {cars.length}</h1>
         </DefaultLayout>
     )
 }
