907aa192cc3f274cc5d35cb79613f54e293097e5
aakaaashmann <aks5060708090100@gmail.com>
2024-06-24 20:02:50 +0530

    registration page completed

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -18,7 +18,7 @@ function Register() {
     <div className="login">
       <Row gutter={16} className="d-flex align-items-center">
         <Col lg={16} style={{ position: "relative" }}>
-          <img src="https://th.bing.com/th/id/R.f37a855707664d48fb9ee46116262cdf?rik=%2fQp3t2zUgWykmw&riu=http%3a%2f%2fpapers.co%2fwallpaper%2fpapers.co-ak64-lamborghini-car-dark-black-awesome-29-wallpaper.jpg&ehk=zb38mkAl2GZPWSwlnHSWnt4%2blf0AJZo2ScMVz09PkOA%3d&risl=&pid=ImgRaw&r=0" />
+          <img src="https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" />
           <h1 className="login-logo">RentACar</h1>
         </Col>
         <Col lg={8} className="text-left p-5">
